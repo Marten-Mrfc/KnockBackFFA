73d2a3d0e27c6d22a9515b2ca3901fc98356d674
Marten Mrfc <101009922+Marten-Mrfc@users.noreply.github.com>
2024-04-04 16:19:32 +0200

    main upload

---
+++ src/main/java/dev/marten_mrfcyt/knockbackffa/KnockBackffA.java
@@ -0,0 +1,17 @@
+package dev.marten_mrfcyt.knockbackffa;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KnockBackffA extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getLogger().info("KnockBackffA has been enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
