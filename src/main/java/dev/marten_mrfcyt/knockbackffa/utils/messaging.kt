02fde232a21e278be924e3c37731da593738dcf1
Marten Mrfc <101009922+Marten-Mrfc@users.noreply.github.com>
2024-04-05 18:37:18 +0200

    Added messaging system
    Error + message = me
    Rest = Gabber235/typewriter

---
+++ src/main/java/dev/marten_mrfcyt/knockbackffa/utils/messaging.kt
@@ -0,0 +1,10 @@
+package dev.marten_mrfcyt.knockbackffa.utils
+import net.kyori.adventure.text.minimessage.MiniMessage
+import org.bukkit.command.CommandSender
+
+private val mm = MiniMessage.builder().build()
+fun String.asMini() = mm.deserialize(this)
+fun CommandSender.sendMini(message: String) = sendMessage(message.asMini())
+
+fun CommandSender.error(message: String) = sendMessage("<red><bold>Error</bold><gray> | <white> $message".asMini())
+fun CommandSender.message(message: String) = sendMessage("<gold><bold>KnockBackFFA</bold><gray> | <white> $message".asMini())
