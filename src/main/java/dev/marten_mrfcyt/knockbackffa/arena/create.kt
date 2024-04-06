18190fca67025ab49d0b4ee36ec68f90534f93ce
Marten Mrfc <101009922+Marten-Mrfc@users.noreply.github.com>
2024-04-06 20:10:01 +0200

    Added create+delete commads adding to arena.yml

--- src/main/java/dev/marten_mrfcyt/knockbackffa/arena/create.kt
+++ src/main/java/dev/marten_mrfcyt/knockbackffa/arena/create.kt
@@ -1,15 +1,37 @@
 package dev.marten_mrfcyt.knockbackffa.arena
 
+import dev.marten_mrfcyt.knockbackffa.utils.asMini
 import dev.marten_mrfcyt.knockbackffa.utils.error
+import dev.marten_mrfcyt.knockbackffa.utils.message
 import dev.marten_mrfcyt.knockbackffa.utils.sendMini
 import org.bukkit.command.CommandSender
+import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.entity.Player
 import org.bukkit.plugin.Plugin
+import java.io.File
 
 fun Plugin.createArena(source: CommandSender, name: String) {
-    source.sendMini("Arena CREATED")
     if (source is Player) {
+    // Setting location and modifying name.
         val location = source.location
+        val arenaName = name.replace(" ", "_")
+        source.message("<green>Creating<white> arena $arenaName at ${"%.1f".format(location.x)}, ${"%.1f".format(location.y)}, ${"%.1f".format(location.z)}")
+    // Save arena to arena.yml
+        val config = File("$dataFolder/arena.yml")
+        if (!config.exists()) {
+            source.message("No arena.yml file found, creating a new one!")
+            try {
+                config.createNewFile()
+            } catch (ex: Exception) {
+                source.error("Failed to create arena.yml file: ${ex.message}")
+                return
+            }
+            source.message("arena.yml file created!")
+        }
+        val arenaConfig = YamlConfiguration.loadConfiguration(config)
+        arenaConfig.set("arenas.$arenaName.location", location)
+        arenaConfig.save(config)
+        source.message("Arena $arenaName <green>created<white> successfully!")
     }
     else {
         source.error("You must be a player to create an arena!")
--- src/main/java/dev/marten_mrfcyt/knockbackffa/arena/delete.kt
+++ src/main/java/dev/marten_mrfcyt/knockbackffa/arena/delete.kt
@@ -1,6 +1,29 @@
 package dev.marten_mrfcyt.knockbackffa.arena
 
-import dev.marten_mrfcyt.knockbackffa.utils.asMini
-import dev.marten_mrfcyt.knockbackffa.utils.sendMini
+import dev.marten_mrfcyt.knockbackffa.utils.error
+import dev.marten_mrfcyt.knockbackffa.utils.message
+import org.bukkit.command.CommandSender
+import org.bukkit.configuration.file.YamlConfiguration
+import org.bukkit.entity.Player
 import org.bukkit.plugin.Plugin
+import java.io.File
 
+fun Plugin.deleteArena(source: CommandSender, name: String) {
+    if (source is Player) {
+        // delete arena from arena.yml
+        source.message("<dark_red>Deleting<white> arena $name!")
+        val config = File("$dataFolder/arena.yml")
+        val arenaConfig = YamlConfiguration.loadConfiguration(config)
+        if (!config.readLines().contains("arenas.$name")) {
+            source.error("Arena $name <dark_red>not found. Is it misspelled<white>!")
+            return
+        }
+        arenaConfig.set("arenas.$name", null)
+        arenaConfig.set("arenas.$name.location", null)
+        arenaConfig.save(config)
+        source.message("Arena $name <dark_red>deleted<white> successfully!")
+    }
+    else {
+        source.error("You must be a player to create an arena!")
+    }
+}
