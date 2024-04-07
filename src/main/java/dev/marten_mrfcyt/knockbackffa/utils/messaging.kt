30694c7d49b909f91a511ecd8613f547d5348d61
Marten Mrfc <101009922+Marten-Mrfc@users.noreply.github.com>
2024-04-07 19:49:20 +0200

    added better message system

--- src/main/java/dev/marten_mrfcyt/knockbackffa/utils/messaging.kt
+++ src/main/java/dev/marten_mrfcyt/knockbackffa/utils/messaging.kt
@@ -8,3 +8,5 @@ fun CommandSender.sendMini(message: String) = sendMessage(message.asMini())
 
 fun CommandSender.error(message: String) = sendMessage("<red><bold>Error</bold><gray> | <white> $message".asMini())
 fun CommandSender.message(message: String) = sendMessage("<gold><bold>KnockBackFFA</bold><gray> | <white> $message".asMini())
+
+fun message(message: String) = "<gold><bold>KnockBackFFA</bold><gray> | <white> $message".asMini()
