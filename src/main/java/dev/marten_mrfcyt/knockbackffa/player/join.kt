28cb2aa8067a4d9ca5020efa0b9093d964d16571
Marten Mrfc <101009922+Marten-Mrfc@users.noreply.github.com>
2024-04-09 18:15:53 +0200

    Removed unused imports

--- src/main/java/dev/marten_mrfcyt/knockbackffa/player/join.kt
+++ src/main/java/dev/marten_mrfcyt/knockbackffa/player/join.kt
@@ -1,19 +1,17 @@
 package dev.marten_mrfcyt.knockbackffa.player
 
+import dev.marten_mrfcyt.knockbackffa.KnockBackFFA
 import dev.marten_mrfcyt.knockbackffa.utils.message
-import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
-import org.bukkit.plugin.Plugin
-import java.util.*
 
 class PlayerJoinListener : Listener {
     @EventHandler
     fun onPlayerJoin(event: PlayerJoinEvent) {
         val source = event.player
         event.joinMessage(message("<white>${source.name}<gray> has joined the arena!"))
-        val scoreboardHandler = ScoreboardHandler()
+        val scoreboardHandler = ScoreboardHandler(KnockBackFFA())
         scoreboardHandler.createScoreboard(source)
     }
 }
--- src/main/java/dev/marten_mrfcyt/knockbackffa/player/scoreboard.kt
+++ src/main/java/dev/marten_mrfcyt/knockbackffa/player/scoreboard.kt
@@ -1,14 +1,11 @@
 package dev.marten_mrfcyt.knockbackffa.player
-import net.kyori.adventure.text.Component
 
 import dev.marten_mrfcyt.knockbackffa.utils.asMini
-import dev.marten_mrfcyt.knockbackffa.utils.message
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.bukkit.scoreboard.Criteria
 import org.bukkit.scoreboard.DisplaySlot
 import org.bukkit.scoreboard.Objective
-import org.bukkit.scoreboard.Scoreboard
 
 class ScoreboardHandler {
     fun createScoreboard(source: Player) {
