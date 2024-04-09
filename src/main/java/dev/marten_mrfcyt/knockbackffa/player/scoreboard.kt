3c065f323e245aef3c09018617ec7c031921ae2c
Marten Mrfc <101009922+Marten-Mrfc@users.noreply.github.com>
2024-04-09 18:16:39 +0200

    Added scoreboard

--- src/main/java/dev/marten_mrfcyt/knockbackffa/player/scoreboard.kt
+++ src/main/java/dev/marten_mrfcyt/knockbackffa/player/scoreboard.kt
@@ -1,5 +1,6 @@
 package dev.marten_mrfcyt.knockbackffa.player
 
+import dev.marten_mrfcyt.knockbackffa.KnockBackFFA
 import dev.marten_mrfcyt.knockbackffa.utils.asMini
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
@@ -7,17 +8,28 @@ import org.bukkit.scoreboard.Criteria
 import org.bukkit.scoreboard.DisplaySlot
 import org.bukkit.scoreboard.Objective
 
-class ScoreboardHandler {
+class ScoreboardHandler(private val plugin: KnockBackFFA) {
+    // Create a map to store custom display names
+    private val displayNames: MutableMap<String, String> = mutableMapOf()
+
     fun createScoreboard(source: Player) {
-        val scoreboard = Bukkit.getScoreboardManager()?.newScoreboard ?: return
+        val scoreboard = Bukkit.getScoreboardManager().newScoreboard
+
+        // Get configuration values
+        val title = plugin.config.getString("scoreboard.title")?.asMini()
+        val lines = plugin.config.getStringList("scoreboard.lines")
 
         // Create objective
-        val objective: Objective = scoreboard.registerNewObjective("test", Criteria.DUMMY, Component.text("Scoreboard"))
+        val objective: Objective = scoreboard.registerNewObjective("test", Criteria.DUMMY, title)
         objective.displaySlot = DisplaySlot.SIDEBAR
 
         // Set scores
-        val score = objective.getScore("Welcome, ${source.name}!")
-        score.score = 1
+        for ((index, line) in lines.withIndex()) {
+            // Convert the line to a mini component
+            val score = objective.getScore("KnockBackFFA_$index")
+            score.customName(line.asMini(source))
+            score.score = lines.size - index
+        }
 
         // Display the scoreboard to the player
         source.scoreboard = scoreboard
