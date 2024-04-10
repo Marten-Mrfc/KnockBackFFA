2ff985433124b5278539babf0374e28faf3d2ef6
Marten Mrfc <101009922+Marten-Mrfc@users.noreply.github.com>
2024-04-10 19:25:16 +0200

    Added join teleport for arenaHandler

--- src/main/java/dev/marten_mrfcyt/knockbackffa/player/join-leave.kt
+++ src/main/java/dev/marten_mrfcyt/knockbackffa/player/join-leave.kt
@@ -1,6 +1,9 @@
 package dev.marten_mrfcyt.knockbackffa.player
 
+import dev.marten_mrfcyt.knockbackffa.KnockBackFFA
+import dev.marten_mrfcyt.knockbackffa.arena.ArenaHandler
 import dev.marten_mrfcyt.knockbackffa.utils.message
+import org.bukkit.Location
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
@@ -12,6 +15,10 @@ class PlayerJoinListener(private val scoreboardHandler: ScoreboardHandler) : Lis
         val source = event.player
         event.joinMessage(message("<white>${source.name}<gray> has joined the arena!"))
         scoreboardHandler.startUpdatingScoreboard(source)
+        val currentArena = KnockBackFFA.instance.config.get("currentLocation") as? Location
+        if (currentArena != null) {
+            source.teleport(currentArena)
+        }
     }
 }
 class PlayerQuitListener(private val scoreboardHandler: ScoreboardHandler) : Listener {
