7635766cb97a2637d2ad7bdf19d294029b14d135
Marten Mrfc <101009922+Marten-Mrfc@users.noreply.github.com>
2024-04-14 20:26:00 +0200

    Added auto-reload + improved performance

--- src/main/java/dev/marten_mrfcyt/knockbackffa/kits/guis/GuiListener.kt
+++ src/main/java/dev/marten_mrfcyt/knockbackffa/kits/guis/GuiListener.kt
@@ -2,9 +2,10 @@ package dev.marten_mrfcyt.knockbackffa.kits.guis
 
 import dev.marten_mrfcyt.knockbackffa.KnockBackFFA
 import dev.marten_mrfcyt.knockbackffa.kits.guis.editor.KitModifier
+import dev.marten_mrfcyt.knockbackffa.utils.asMini
 import dev.marten_mrfcyt.knockbackffa.utils.checkCustomValue
+import dev.marten_mrfcyt.knockbackffa.utils.error
 import dev.marten_mrfcyt.knockbackffa.utils.getCustomValue
-import dev.marten_mrfcyt.knockbackffa.utils.message
 import org.bukkit.Material
 import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.entity.Player
@@ -20,82 +21,51 @@ class GuiListener(private val plugin: KnockBackFFA) : Listener {
 
     @EventHandler
     fun onInventoryClick(event: InventoryClickEvent) {
-        val clickedInventory = event.clickedInventory
         val clickedItem = event.currentItem ?: return
-        if (checkCustomValue(clickedItem.itemMeta, plugin, "is_draggable", false)) {
-            event.isCancelled = true
-        } else {
-            if (event.whoClicked is Player) {
-                val source = event.whoClicked as Player
-                source.message("You clicked on $clickedItem")
+        val source = event.whoClicked as? Player ?: return
+        val clickedInventory = event.clickedInventory
+
+        when {
+            checkCustomValue(clickedItem.itemMeta, plugin, "is_draggable", false) -> {
+                event.isCancelled = true
             }
-            // Check if the clicked inventory is the correct one
-            if (clickedInventory != null && clickedInventory == event.whoClicked.openInventory.topInventory) {
-                // Check if the clicked item is a kit display item
-                if (checkCustomValue(
-                        clickedItem.itemMeta,
-                        plugin,
-                        "6B69745F646973706C61795F6974656D",
-                        "kit_display_item_check"
-                    )
-                ) {
-                    println("Custom value check passed for kit_display_item")
-                    event.isCancelled = true
-                    if (event.whoClicked is Player) {
-                        val source = event.whoClicked as Player
-                        source.message("Custom value check passed for kit_display_item")
+            clickedInventory != null && clickedInventory == event.whoClicked.openInventory.topInventory -> {
+                when {
+                    checkCustomValue(clickedItem.itemMeta, plugin, "6B69745F646973706C61795F6974656D", "kit_display_item_check") -> {
+                        event.isCancelled = true
                         val itemInHand = event.cursor
-                        // Check if the item in hand is not null and not air
+                        val kitName = getCustomValue(clickedItem.itemMeta, plugin, "kit_display_item") as String
+                        val kitLore = kitConfig.getConfigurationSection("kit.$kitName")?.getString("Lore")
                         if (itemInHand.type != Material.AIR) {
-                            println("Item in hand is not null and not air")
-                            source.message("Item in hand is not null and not air")
                             val itemType = itemInHand.type.name
-                            println("Item type: $itemType")
                             val itemEnchants = itemInHand.enchantments.map { it.key.key to it.value }.toMap()
 
-                            // Get the kit section for the clicked item
-                            val kitName = getCustomValue(clickedItem.itemMeta, plugin, "kit_display_item") as String
-                            val kitSection = kitConfig.getConfigurationSection("kit.$kitName")
-
-                            // Set the new item and enchantments
-                            println("Current DisplayItem: ${kitSection?.getConfigurationSection("DisplayItem")?.getString("item")}")
-                            kitSection?.getConfigurationSection("DisplayItem")?.set("item", itemType)
-                            kitSection?.getConfigurationSection("DisplayItem")?.getConfigurationSection("enchants")
-                                ?.let { enchantsSection ->
-                                    enchantsSection.getKeys(false)
-                                        .forEach { enchantsSection.set(it, null) } // Clear existing enchantments
+                            kitConfig.getConfigurationSection("kit.$kitName")?.apply {
+                                getConfigurationSection("DisplayItem")?.set("item", itemType)
+                                getConfigurationSection("DisplayItem")?.getConfigurationSection("enchants")?.let { enchantsSection ->
+                                    enchantsSection.getKeys(false).forEach { enchantsSection.set(it, null) }
                                     itemEnchants.forEach { (enchant, level) ->
-                                        val enchantKey =
-                                            enchant.toString().split(":").last().uppercase(Locale.getDefault()) // Get only the key part after the namespace and convert it to uppercase
-                                        enchantsSection.set(
-                                            enchantKey,
-                                            level
-                                        )
+                                        val enchantKey = enchant.toString().split(":").last().uppercase(Locale.getDefault())
+                                        enchantsSection.set(enchantKey, level)
                                     }
                                 }
-                            println("New item and enchantments set: $itemType, $itemEnchants")
-                            source.message("New item and enchantments set")
+                            } ?: source.error("Kit section could not be found.")
                         }
                         kitConfig.save(config)
-                        println("Kit config saved")
-                        source.message("Kit config saved")
+                        if (kitLore != null) {
+                            KitModifier(plugin).openNewKitGUI(source,kitName.asMini(),kitLore.asMini(), false)
+                        }
+
                     }
-                }
-                if (checkCustomValue(
-                        clickedItem.itemMeta,
-                        plugin,
-                        "6F70656E5F6B69745F656469746F72",
-                        "open_kit_editor"
-                    )
-                ) {
-                    event.isCancelled = true
-                    if (event.whoClicked is Player) {
-                        val source = event.whoClicked as Player
-                        val name = clickedItem.itemMeta.displayName()
-                        val lore = clickedItem.itemMeta.lore()?.get(0)
-                        if (name != null) {
-                            if (lore != null) {
+                    checkCustomValue(clickedItem.itemMeta, plugin, "6F70656E5F6B69745F656469746F72", "open_kit_editor") -> {
+                        event.isCancelled = true
+                        clickedItem.itemMeta?.let { itemMeta ->
+                            val name = itemMeta.displayName()
+                            val lore = itemMeta.lore()?.get(0)
+                            if (name != null && lore != null) {
                                 KitModifier(plugin).openNewKitGUI(source, name, lore, false)
+                            } else {
+                                source.error("Name or lore could not be found.")
                             }
                         }
                     }
