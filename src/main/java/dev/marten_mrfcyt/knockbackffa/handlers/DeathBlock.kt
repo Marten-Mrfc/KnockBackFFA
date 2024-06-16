b1cd8d2d61868ea22ef45f3b3f36bae8b73b7e85
Marten Mrfc <101009922+Marten-Mrfc@users.noreply.github.com>
2024-06-16 19:19:48 +0200

    Deathblock got error handling and null checks

--- src/main/java/dev/marten_mrfcyt/knockbackffa/handlers/DeathBlock.kt
+++ src/main/java/dev/marten_mrfcyt/knockbackffa/handlers/DeathBlock.kt
@@ -1,6 +1,7 @@
 package dev.marten_mrfcyt.knockbackffa.handlers
 
 import dev.marten_mrfcyt.knockbackffa.KnockBackFFA
+import org.bukkit.Location
 import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
@@ -9,14 +10,17 @@ import org.bukkit.event.player.PlayerMoveEvent
 import java.io.File
 
 class DeathBlock(private val plugin: KnockBackFFA) : Listener {
-    @EventHandler(priority = EventPriority.HIGHEST)
+    private val arena = plugin.config.get("currentArena") as? String
+    val config = YamlConfiguration.loadConfiguration(File("${plugin.dataFolder}/arena.yml"))
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     fun onDeathBlock(event: PlayerMoveEvent) {
-        val source = event.player
-        val arena = plugin.config.get("currentArena")
-        val config = YamlConfiguration.loadConfiguration(File("${plugin.dataFolder}/arena.yml"))
+        if (arena == null) {
+            return
+        }
         val killBlock = config.get("arenas.$arena.killBlock") as String
+        val source = event.player
         if (source.location.add(0.0, -0.425, 0.0).block.type.name == killBlock) {
-            source.damage(1000.0)
+            source.health = 0.0
         }
     }
 }
