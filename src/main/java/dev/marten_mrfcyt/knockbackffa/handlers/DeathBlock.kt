c25e46c1bd043a6e4c39c126dca27327bcac7377
Marten Mrfc <101009922+Marten-Mrfc@users.noreply.github.com>
2024-06-17 20:28:43 +0200

    Deathblock event checks

--- src/main/java/dev/marten_mrfcyt/knockbackffa/handlers/DeathBlock.kt
+++ src/main/java/dev/marten_mrfcyt/knockbackffa/handlers/DeathBlock.kt
@@ -10,17 +10,18 @@ import org.bukkit.event.player.PlayerMoveEvent
 import java.io.File
 
 class DeathBlock(private val plugin: KnockBackFFA) : Listener {
-    private val arena = plugin.config.get("currentArena") as? String
     val config = YamlConfiguration.loadConfiguration(File("${plugin.dataFolder}/arena.yml"))
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGHEST)
     fun onDeathBlock(event: PlayerMoveEvent) {
-        if (arena == null) {
-            return
-        }
+        println("DeathBlock event")
+        val arena = plugin.config.get("currentArena") as? String ?: return
         val killBlock = config.get("arenas.$arena.killBlock") as String
         val source = event.player
+        println("$killBlock, ${source.location.add(0.0, -0.425, 0.0).block.type.name}, ${source.location.add(0.0, -0.425, 0.0).block.type.name == killBlock},$arena,${config.get("arenas.$arena.killBlock")} ")
         if (source.location.add(0.0, -0.425, 0.0).block.type.name == killBlock) {
             source.health = 0.0
+            source.damage(100.0)
+            println("Player died")
         }
     }
 }
