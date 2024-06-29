b17551024768b30d5fa1aab635517fa7c6e87b2d
Marten Mrfc <101009922+Marten-Mrfc@users.noreply.github.com>
2024-06-29 22:25:04 +0200

    [Fix] Deathblock name not capital always

--- src/main/java/dev/marten_mrfcyt/knockbackffa/handlers/DeathBlock.kt
+++ src/main/java/dev/marten_mrfcyt/knockbackffa/handlers/DeathBlock.kt
@@ -13,15 +13,12 @@ class DeathBlock(private val plugin: KnockBackFFA) : Listener {
     val config = YamlConfiguration.loadConfiguration(File("${plugin.dataFolder}/arena.yml"))
     @EventHandler(priority = EventPriority.HIGHEST)
     fun onDeathBlock(event: PlayerMoveEvent) {
-        println("DeathBlock event")
         val arena = plugin.config.get("currentArena") as? String ?: return
         val killBlock = config.get("arenas.$arena.killBlock") as String
         val source = event.player
-        println("$killBlock, ${source.location.add(0.0, -0.425, 0.0).block.type.name}, ${source.location.add(0.0, -0.425, 0.0).block.type.name == killBlock},$arena,${config.get("arenas.$arena.killBlock")} ")
-        if (source.location.add(0.0, -0.425, 0.0).block.type.name == killBlock) {
+        if (source.location.add(0.0, -0.425, 0.0).block.type.name == killBlock.uppercase()) {
             source.health = 0.0
             source.damage(100.0)
-            println("Player died")
         }
     }
 }
