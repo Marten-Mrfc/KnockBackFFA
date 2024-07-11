34c4c15d9f07f42388583ad5ebd29399ae645a56
Marten Mrfc <101009922+Marten-Mrfc@users.noreply.github.com>
2024-07-11 20:34:39 +0200

    Deathblock got fixed

--- src/main/java/dev/marten_mrfcyt/knockbackffa/handlers/DeathBlock.kt
+++ src/main/java/dev/marten_mrfcyt/knockbackffa/handlers/DeathBlock.kt
@@ -15,10 +15,13 @@ class DeathBlock(private val plugin: KnockBackFFA) : Listener {
     @EventHandler(priority = EventPriority.HIGHEST)
     fun onDeathBlock(event: PlayerMoveEvent) {
         val arena = plugin.config.getString("currentArena") ?: return
-        val killBlock = config.getString("arenas.$arena.killBlock")?.uppercase()
+        val killBlock = config.getString("arenas.$arena.killBlock")?.replace("minecraft:", "", ignoreCase = true)?.uppercase()
+        // Adjust the Y-coordinate by subtracting the desired amount
+        val adjustedLocation = event.to.clone().add(0.0, -0.245, 0.0)
+        val blockBelowPlayer = adjustedLocation.block
 
-        // Ensure killBlock is not null before proceeding
-        if (killBlock != null && event.to.block.type.name.uppercase() == killBlock) {
+        println("$arena = $killBlock = ${blockBelowPlayer.type.name}")
+        if (killBlock != null && blockBelowPlayer.type.name.equals(killBlock, ignoreCase = true)) {
             event.player.health = 0.0
             event.player.damage(100.0)
         }
