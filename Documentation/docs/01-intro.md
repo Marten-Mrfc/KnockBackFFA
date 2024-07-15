137e6e4b1a4fe86422037205ed738e72b6253467
Marten Mrfc <101009922+Marten-Mrfc@users.noreply.github.com>
2024-07-15 20:44:44 +0200

    [Docs] Added all needed docs for publish

---
+++ Documentation/docs/01-intro.md
@@ -0,0 +1,13 @@
+---
+sidebar_position: 1
+---
+
+# Introduction
+KnockBackFFA is a PVP minigame where players need to throw each other off a map into the void or on an arena specific block.
+
+**Features**:
+
+- **Arenas**: Create, modify and delete arena's using commands or via the config
+- **Kits**: Create, modify and delete kits via GUI or in the config.
+- **Scoreboard**: Have a scoreboard displaying all the stats of the game to your players using the PlaceHolderAPI plugin
+- **Custom messages**: Most messages players get send can be modified in the messages.yml
--- Documentation/docusaurus.config.js
+++ Documentation/docusaurus.config.js
@@ -44,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Marten-Mrfc/KnockBackFFA/tree/master/Documentation/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Marten-Mrfc/KnockBackFFA/tree/master/Documentation/',
         },
         theme: {
           customCss: './src/css/custom.css',
